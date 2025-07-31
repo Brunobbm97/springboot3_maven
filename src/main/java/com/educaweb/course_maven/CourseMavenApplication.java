cdf5c3c2cf2044a20d91c992061608064703d020
Bruno Machado <brunobbmachado97@outlook.com>
2025-07-31 14:41:46 -0300

    first commit

---
+++ src/main/java/com/educaweb/course_maven/CourseMavenApplication.java
@@ -0,0 +1,13 @@
+package com.educaweb.course_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educaweb/course_maven/CourseMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educaweb.course_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
